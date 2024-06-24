a9fdd7d91e43bfb964b11ea8651250baee0ca2e6
Shubham Matole <shubhammatole1@gmail.com>
2024-06-24 12:09:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
    $(window).on('load scroll',function(){
        $('.fa-bars').removeClass('fa-times');
        $('.navbar').removeClass('nav-toggle');
 
        if($(window).scrollTop()>35)
        {
-           $('.header').css({'background':'#7D7D7C','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+           $('.header').css({'background':'#e1dbdb','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
            $('.header .navbar ul li a').css({'color':'#3461c1'});
            $('.navbar ul li a:hover').css({'color':'#00bfff'});
        }
